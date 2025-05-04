dc08677226031c2506bcf7645838b84eff125657
Meehoi <beam_baimon@hotmail.com>
2025-05-04 16:41:23 +0700

    improve mobile perf by unscaled it

--- components/map.ts
+++ components/map.ts
@@ -1,8 +1,22 @@
-import centerSvg from './center.svg';
-
-function scaled(x: number) {
-  return x * window.devicePixelRatio;
+export function isMobileAndTablet() {
+  /* eslint-disable */
+  let check = false;
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+        a,
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(-|_)|g1 u|g560|gene|gf-5|g-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd-(m|p|t)|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c(-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac( |-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c(-|0|1)|47|mc|nd|ri)|sgh-|shar|sie(-|m)|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel(i|m)|tim-|t-mo|to(pl|sh)|ts(70|m-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(
+        a.substr(0, 4),
+      )
+    )
+      check = true;
+  })(navigator.userAgent || navigator.vendor || (window as any).opera);
+  return check;
+  /* eslint-enable */
 }
+
+import centerSvg from './center.svg';
 
 export type Points = {
   position: Vector2;
@@ -79,24 +93,30 @@ export class MotorTownMap extends HTMLElement {
     super();
   }
 
+  private isMobileAndTablet = false;
+
+  private scaled(x: number) {
+    return x * (this.isMobileAndTablet ? 1 : window.devicePixelRatio);
+  }
+
   private getMarkerScale() {
-    const markerScale = scaled(10 / this.currentScale);
+    const markerScale = this.scaled(10 / this.currentScale);
     return markerScale;
   }
 
-  private baseCheckpointRadius = () => scaled(0.5);
-  private baseArrowLength = () => scaled(2);
-  private baseArrowHeadLength = () => scaled(1);
-  private baseConnectingLineWidth = () => scaled(0.2);
-  private baseArrowLineWidth = () => scaled(0.2);
-  private baseLabelFontSize = () => scaled(0.6);
-  private selectRadius = () => this.baseCheckpointRadius() + scaled(0.5);
+  private baseCheckpointRadius = () => this.scaled(0.5);
+  private baseArrowLength = () => this.scaled(2);
+  private baseArrowHeadLength = () => this.scaled(1);
+  private baseConnectingLineWidth = () => this.scaled(0.2);
+  private baseArrowLineWidth = () => this.scaled(0.2);
+  private baseLabelFontSize = () => this.scaled(0.6);
+  private selectRadius = () => this.baseCheckpointRadius() + this.scaled(0.5);
 
   private selectedIndex: number | undefined = undefined;
   private hoveredIndex: number | undefined = undefined;
 
   private getSelectRadius() {
-    const selectRadius = (this.baseCheckpointRadius() + scaled(0.5)) * this.getMarkerScale();
+    const selectRadius = (this.baseCheckpointRadius() + this.scaled(0.5)) * this.getMarkerScale();
     return selectRadius;
   }
 
@@ -148,7 +168,7 @@ export class MotorTownMap extends HTMLElement {
     if (this.points) {
       const markerScale = this.getMarkerScale();
       // Draw each waypoint with its arrow and label.
-      const lineWidth = scaled(0.1 * markerScale);
+      const lineWidth = this.scaled(0.1 * markerScale);
       const radius = this.baseCheckpointRadius() * markerScale;
       const arrowLength = this.baseArrowLength();
       const arrowHeadLength = this.baseArrowHeadLength();
@@ -241,16 +261,16 @@ export class MotorTownMap extends HTMLElement {
       });
       const deltaX = maxX - minX;
       const deltaY = maxY - minY;
-      const currentScaleX = (this.mapCanvas.width - scaled(FIT_PADDING * 2)) / deltaX;
-      const currentScaleY = (this.mapCanvas.height - scaled(FIT_PADDING * 2)) / deltaY;
+      const currentScaleX = (this.mapCanvas.width - this.scaled(FIT_PADDING * 2)) / deltaX;
+      const currentScaleY = (this.mapCanvas.height - this.scaled(FIT_PADDING * 2)) / deltaY;
       if (currentScaleX < currentScaleY) {
         this.currentScale = Math.min(40, currentScaleX);
-        this.offsetX = -minX * this.currentScale + scaled(FIT_PADDING);
+        this.offsetX = -minX * this.currentScale + this.scaled(FIT_PADDING);
         const midpointY = (minY + maxY) / 2;
         this.offsetY = -midpointY * this.currentScale + this.mapCanvas.height / 2;
       } else {
         this.currentScale = Math.min(40, currentScaleY);
-        this.offsetY = -minY * this.currentScale + scaled(FIT_PADDING);
+        this.offsetY = -minY * this.currentScale + this.scaled(FIT_PADDING);
         const midpointX = (minX + maxX) / 2;
         this.offsetX = -midpointX * this.currentScale + this.mapCanvas.width / 2;
       }
@@ -320,16 +340,16 @@ export class MotorTownMap extends HTMLElement {
       const dy = currentY - this.lastY;
       this.lastX = currentX;
       this.lastY = currentY;
-      this.offsetX += scaled(dx);
-      this.offsetY += scaled(dy);
+      this.offsetX += this.scaled(dx);
+      this.offsetY += this.scaled(dy);
       this.updateRecenterBtn(false);
       this.drawMap();
       return;
     }
 
     if (this.points) {
-      const hoverX = (scaled(clientX) - scaled(rect.left) - this.offsetX) / this.currentScale;
-      const hoverY = (scaled(clientY) - scaled(rect.top) - this.offsetY) / this.currentScale;
+      const hoverX = (this.scaled(clientX) - this.scaled(rect.left) - this.offsetX) / this.currentScale;
+      const hoverY = (this.scaled(clientY) - this.scaled(rect.top) - this.offsetY) / this.currentScale;
       const selectRadius = this.getSelectRadius();
 
       let hoveredIndex: number | undefined;
@@ -388,8 +408,8 @@ export class MotorTownMap extends HTMLElement {
 
   private resizeEvent = () => {
     const canvasBound = this.mapCanvas.getBoundingClientRect();
-    this.mapCanvas.width = scaled(canvasBound.width);
-    this.mapCanvas.height = scaled(canvasBound.height);
+    this.mapCanvas.width = this.scaled(canvasBound.width);
+    this.mapCanvas.height = this.scaled(canvasBound.height);
     if (this.points) {
       this.setPoint(this.points);
     }
@@ -402,6 +422,7 @@ export class MotorTownMap extends HTMLElement {
   };
 
   protected connectedCallback() {
+    this.isMobileAndTablet = isMobileAndTablet();
     const shadow = this.attachShadow({ mode: 'open' });
 
     this.mapImage.onload = () => {
@@ -462,8 +483,8 @@ export class MotorTownMap extends HTMLElement {
     shadow.appendChild(this.recenterButton);
     shadow.appendChild(this.mapCanvas);
     const canvasBound = this.mapCanvas.getBoundingClientRect();
-    this.mapCanvas.width = scaled(canvasBound.width);
-    this.mapCanvas.height = scaled(canvasBound.height);
+    this.mapCanvas.width = this.scaled(canvasBound.width);
+    this.mapCanvas.height = this.scaled(canvasBound.height);
   }
 
   disconnectedCallback() {
